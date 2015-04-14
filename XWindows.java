17bd398aa258dabd0e0dcd0da1590ed58f30b058
Thomas Gundlach <Thomas.Gundlach@HAW-Hamburg.de>
2015-04-14 16:52:03 +0200

    added user input

--- XWindows.java
+++ XWindows.java
@@ -3,5 +3,9 @@ import java.util.*;
 public class XWindows {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+		System.out.print("Bitte eingeben [+-*/] zahl1 zahl2: ");
+		String operator = scanner.next();
+		int zahl1 = scanner.nextInt();
+		int zahl2 = scanner.nextInt();
 	}
 }
